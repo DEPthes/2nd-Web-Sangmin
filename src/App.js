93f323a707601d83592af2baa6dbd4c059d7b4df
박상민 <matthew10164@gmail.com>
2023-06-25 03:19:38 +0900

    week4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useCallback } from 'react';
-import TodoInsert from './components/TodoInstert';
+import TodoInsert from './components/TodoInsert';
 import TodoTemplate from './components/TodoTemplate';
 import TodoList from './components/TodoList';
 
